b45a9bd00d504beed948e11fa0f1d2f44de23e03
srciaga <srciaga@gmail.com>
2022-01-07 00:45:36 -0800

    Styled site and protected new entry page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -57,6 +57,8 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # No data submitted; create a blank form.
